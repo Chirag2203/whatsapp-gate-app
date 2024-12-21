80a6455ef6f3b3fc185d15f0af4cf5193533f51a
pranav-salunkhe <pvsalunkhe2003@gmail.com>
2024-12-21 19:38:03 +0530

    daily challenge

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -953,6 +953,68 @@ async function handlePost(req, res) {
                         return res.sendStatus(200);
                     }
                 }
+            } else if(msg_body == "/feedback" || (current_msg.context && current_msg.context.id == userState.feedbackMsgId)){
+                if(current_msg.context && current_msg.context.id == userState.feedbackMsgId){
+                    if(current_msg.type == "interactive"){
+                        fb = JSON.parse(current_msg.interactive.nfm_reply.response_json);
+                        function formatRating(value) {
+                            const parts = value.split("_•_");
+                            const stars = parts[0].split("_")[1]; // Extract stars (★★★★★)
+                            const description = parts[1]?.split(" ")[0]; // Extract description (e.g., "Excellent")
+                            return `${stars} - ${description}`;
+                        }
+                        const feedback = {
+                            questionVariety: formatRating(fb.screen_1_Question_Variety_0),
+                            explanationQuality: formatRating(fb.screen_1_Explanation_Quality_1),
+                            easeOfInteraction: formatRating(fb.screen_1_Ease_of_Interaction_2),
+                            usefulness: formatRating(fb.screen_1_Usefulness_3),
+                            choose: fb.screen_0_Choose_0.split("_")[1],
+                            featureImprovement: fb.screen_0_Suggest_a_feature_improvement_1,
+                        };
+                        userState.feedback = feedback;
+                        console.log("feedback:", userState.feedback );
+                        // if(feedback == "user_feedback"){
+                        await sendMessage(from, "Thank you for your feedback! 🙏", phon_no_id);
+                        // }
+                        await updateUserState(from, userState);
+                    }
+                }else{
+                const feedbackResp = await axios({
+                    method: "POST",
+                    url: `https://graph.facebook.com/v21.0/${phon_no_id}/messages`,
+                    data: {
+                        "messaging_product": "whatsapp",
+                        "to": `${from}`,
+                        "type": "template",
+                        "template": {
+                            "name": "user_feedback",
+                            "language": {
+                              "code": "en"
+                            },
+                            "components": [
+                              {
+                                "type": "button",
+                                "sub_type": "flow",
+                                "index": "0",
+                                "parameters": [
+                                  {
+                                    "type": "action",
+                                    "action": {}
+                                  }
+                                ]
+                              }
+                            ]
+                        },
+                    },
+                    headers: {
+                        "Authorization": `Bearer ${PERMANENT_TOKEN}`,
+                        "Content-Type": "application/json",
+                    },
+                });
+                userState.feedbackMsgId = feedbackResp.data.messages[0].id;
+                
+                await updateUserState(from, userState);
+                }
             } 
 
             }
