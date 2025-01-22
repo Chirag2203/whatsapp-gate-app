09d5f74c13c9e76caa4d6eea267c67bba1e23aac
pranav-salunkhe <pvsalunkhe2003@gmail.com>
2025-01-22 19:03:44 +0530

    add feature ask - integrate img api

--- src/utils/webhook/askConversation.js
+++ src/utils/webhook/askConversation.js
@@ -7,20 +7,20 @@ const BACKEND_URL = "https://kalppo-backend.vercel.app";
 const WHATSAPP_SECRET_KEY = process.env.WHATSAPP_BACKEND_SECRET;
 
 async function askConversation(userState, body_param, from, phon_no_id){
-    if(!userState.jwt){
-        const getTokenData = {
-            phoneNumber: userState.phoneNumber,
-            secretKey: WHATSAPP_SECRET_KEY,
-        };
-        const tokenResponse = await axios.post(`${BACKEND_URL}/auth/token/get`, getTokenData, {
-          headers: {
-            'content-type': 'application/json'
-          }
-        });
-        const jwtToken = tokenResponse.data.jwtToken;
-        userState.jwt = jwtToken;
-        await updateUserState(from, userState);
-    }
+    // if(!userState.jwt){
+    const getTokenData = {
+        phoneNumber: userState.phoneNumber,
+        secretKey: WHATSAPP_SECRET_KEY,
+    };
+    const tokenResponse = await axios.post(`${BACKEND_URL}/auth/token/get`, getTokenData, {
+        headers: {
+        'content-type': 'application/json'
+        }
+    });
+    const jwtToken = tokenResponse.data.jwtToken;
+    userState.jwt = jwtToken;
+    await updateUserState(from, userState);
+    // }
 
     if(body_param.entry[0].changes[0].value.messages[0].type == "image"){
         console.log("inside ask conv (image)")
